e53cc3cc3a06920569fba22c4a3bc9c4b052b27b
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2023-02-08 18:20:27 +0200

    bdd-pom

--- pages/ebay_advanced_search_page.py
+++ pages/ebay_advanced_search_page.py
@@ -11,18 +11,18 @@ class Advanced_search_page(Base_page):
 
 
 def enter_keywords_or_item_number(self, product):
-	self.chrome.find_element(*self.ENTER_KEYWORDS_OR_ITEM_NUMBER).send_keys(product)
+	self.driver.find_element(*self.ENTER_KEYWORDS_OR_ITEM_NUMBER).send_keys(product)
 
 def select_keyword_options(self):
-	keyword_dropdown = Select(self.chrome.find_element(*self.KEYWORD_OPTIONS))
+	keyword_dropdown = Select(self.driver.find_element(*self.KEYWORD_OPTIONS))
 	keyword_dropdown.select_by_visible_text("Exact words, exact order")
 
 def select_search_category(self, category):
-	category_dropdown = Select(self.chrome.find_element(*self.SEARCH_CATEGORIES))
+	category_dropdown = Select(self.driver.find_element(*self.SEARCH_CATEGORIES))
 	category_dropdown.select_by_visible_text(category)
 
 def exclude_word_from_search(self, exclude_product):
-	self.chrome.find_element(*self.EXCLUDE_WORDS_FROM_SEARCH).send_keys(exclude_product)
+	self.driver.find_element(*self.EXCLUDE_WORDS_FROM_SEARCH).send_keys(exclude_product)
 
 def click_search_button(self):
 	self.wait_and_click_element_by_selector(*self.SEARCH_BUTTON)
