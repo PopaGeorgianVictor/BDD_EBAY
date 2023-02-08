37855dbb7aa1fa632310fe67f0299fd8127d1436
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2023-02-08 18:14:25 +0200

    bdd-pom

--- pages/base_page.py
+++ pages/base_page.py
@@ -8,10 +8,10 @@ class Base_page(Browser):
 		COOKIES_BUTTON = (By.ID,"gdpr-banner-accept")
 		def accept_cookies(self):
 				try:
-						self.chrome.find_element(*self.COOKIES_BUTTON).click()
+						self.driver.find_element(*self.COOKIES_BUTTON).click()
 				except:
 						pass
 
 		def wait_and_click_element_by_selector(self, by, selector):
-				WebDriverWait(self.chrome, 5).until(EC.presence_of_element_located((by, selector)))
-				self.chrome.find_element(by,selector).click()
+				WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((by, selector)))
+				self.driver.find_element(by,selector).click()
