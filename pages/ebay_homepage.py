aa2b562ccc92f8d6b713412b39796b3b9f3e27a0
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2023-02-08 18:29:59 +0200

    bdd-pom

--- pages/ebay_homepage.py
+++ pages/ebay_homepage.py
@@ -12,20 +12,20 @@ class Home_page(Base_page):
 		SEARCH_RESULTS = (By.XPATH,'//h1/span[@class="BOLD"][1]')
 
 		def  navigate_to_homepage(self):
-				self.chrome.get("https://www.ebay.com/")
+				self.driver.get("https://www.ebay.com/")
 
 		def insert_search_value(self,product_name):
-				self.chrome.find_element(*self.SEARCH_TEXTBOX).send_keys(product_name)
+				self.driver.find_element(*self.SEARCH_TEXTBOX).send_keys(product_name)
 
 		def choose_category(self,category_name):
-				category_dropdown = Select(self.chrome.find_element(*self.SEARCH_CATEGORIES))
+				category_dropdown = Select(self.driver.find_element(*self.SEARCH_CATEGORIES))
 				category_dropdown.select_by_visible_text(category_name)
 
 		def click_search_button(self):
-				self.chrome.find_element(*self.SEARCH_BUTTON).click()
+				self.driver.find_element(*self.SEARCH_BUTTON).click()
 
 		def check_search_results(self,no_of_results):
-				baseResult = self.chrome.find_element(*self.SEARCH_RESULTS).text
+				baseResult = self.driver.find_element(*self.SEARCH_RESULTS).text
 				result = baseResult.replace(",","")
 				assert int(result)>=int(no_of_results), f"Error: Results are incorrect. Expected: >{no_of_results}, Actual: {result}"
 
